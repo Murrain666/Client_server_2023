89e76b37702ec18c0aaf0e7211da5865a0c3411e
Margarit <60291184+Murrain666@users.noreply.github.com>
2023-12-03 19:49:17 +0300

    Add files via upload

---
+++ Client/MainForm.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace Client
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ApplyButton = new System.Windows.Forms.Button();
+            this.TextLabel = new System.Windows.Forms.Label();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.MyTextBox = new System.Windows.Forms.TextBox();
+            this.AllTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // ApplyButton
+            // 
+            this.ApplyButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ApplyButton.BackColor = System.Drawing.Color.Plum;
+            this.ApplyButton.Location = new System.Drawing.Point(387, 216);
+            this.ApplyButton.Name = "ApplyButton";
+            this.ApplyButton.Size = new System.Drawing.Size(75, 23);
+            this.ApplyButton.TabIndex = 13;
+            this.ApplyButton.Text = "Применить";
+            this.ApplyButton.UseVisualStyleBackColor = false;
+            this.ApplyButton.Click += new System.EventHandler(this.ApplyButton_Click);
+            // 
+            // TextLabel
+            // 
+            this.TextLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextLabel.AutoSize = true;
+            this.TextLabel.Location = new System.Drawing.Point(12, 247);
+            this.TextLabel.Name = "TextLabel";
+            this.TextLabel.Size = new System.Drawing.Size(100, 13);
+            this.TextLabel.TabIndex = 12;
+            this.TextLabel.Text = "Текст сообщения:";
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(12, 221);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(106, 13);
+            this.NameLabel.TabIndex = 11;
+            this.NameLabel.Text = "Имя пользователя:";
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.NameTextBox.Location = new System.Drawing.Point(118, 218);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(263, 20);
+            this.NameTextBox.TabIndex = 10;
+            // 
+            // SendButton
+            // 
+            this.SendButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SendButton.BackColor = System.Drawing.Color.SpringGreen;
+            this.SendButton.Enabled = false;
+            this.SendButton.Location = new System.Drawing.Point(15, 270);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(450, 33);
+            this.SendButton.TabIndex = 9;
+            this.SendButton.Text = "Отправить";
+            this.SendButton.UseVisualStyleBackColor = false;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // MyTextBox
+            // 
+            this.MyTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MyTextBox.Enabled = false;
+            this.MyTextBox.Location = new System.Drawing.Point(118, 244);
+            this.MyTextBox.Name = "MyTextBox";
+            this.MyTextBox.Size = new System.Drawing.Size(344, 20);
+            this.MyTextBox.TabIndex = 8;
+            // 
+            // AllTextBox
+            // 
+            this.AllTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.AllTextBox.Location = new System.Drawing.Point(12, 12);
+            this.AllTextBox.Name = "AllTextBox";
+            this.AllTextBox.ReadOnly = true;
+            this.AllTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.AllTextBox.Size = new System.Drawing.Size(450, 200);
+            this.AllTextBox.TabIndex = 7;
+            this.AllTextBox.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PapayaWhip;
+            this.ClientSize = new System.Drawing.Size(474, 305);
+            this.Controls.Add(this.ApplyButton);
+            this.Controls.Add(this.TextLabel);
+            this.Controls.Add(this.NameLabel);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.MyTextBox);
+            this.Controls.Add(this.AllTextBox);
+            this.MinimumSize = new System.Drawing.Size(490, 344);
+            this.Name = "MainForm";
+            this.Text = "Окно клиента";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ApplyButton;
+        private System.Windows.Forms.Label TextLabel;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.TextBox MyTextBox;
+        private System.Windows.Forms.RichTextBox AllTextBox;
+    }
+}
+
---
+++ Client/MainForm.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// Имя клиента.
+        /// </summary>
+        string userName;
+
+        /// <summary>
+        /// Адрес сервера.
+        /// </summary>
+        const string host = "127.0.0.1";
+
+        /// <summary>
+        /// Порт сервера.
+        /// </summary>
+        const int port = 8005;
+
+        TcpClient client;
+        NetworkStream stream;
+
+        /// <summary>
+        /// Конструктор класса MainForm.
+        /// </summary>
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Метод подключения к серверу.
+        /// </summary>
+        private void Connection()
+        {
+            client = new TcpClient();
+            try
+            {
+                client.Connect(host, port);  // подключение клиента
+                stream = client.GetStream(); // получаем поток
+
+                string message = userName;
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+
+                // запускаем новый поток для получения данных
+                Thread receiveThread = new Thread(new ThreadStart(ReceiveMessage))
+                {
+                    IsBackground = true
+                };
+                receiveThread.Start(); //старт потока
+                AllTextBox.Text = string.Format("Здраствуй, {0} \n", userName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Внимание!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        /// <summary>
+        /// Метод отправки сообщений.
+        /// </summary>
+        private void SendMessage()
+        {
+            string message = MyTextBox.Text;
+            AllTextBox.Text += message + "\n";
+            MyTextBox.Text = string.Empty;
+            if (message != string.Empty)
+            {
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+            }
+        }
+
+        /// <summary>
+        /// Метод получения сообщений.
+        /// </summary>
+        private void ReceiveMessage()
+        {
+            while (true)
+            {
+                try
+                {
+                    byte[] data = new byte[64];
+                    StringBuilder builder = new StringBuilder();
+                    do
+                    {
+                        int bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+
+                    string message = builder.ToString();
+
+                    if (AllTextBox.InvokeRequired)
+                        AllTextBox.Invoke(new Action<string>((s) => AllTextBox.AppendText(s + "\n")), message);
+                    else
+                        AllTextBox.AppendText(message);
+                }
+                catch
+                {
+                    MessageBox.Show("Подключение прервано!", "Внимание!",
+                        MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    Disconnect();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Метод отключения от сервера.
+        /// </summary>
+        private void Disconnect()
+        {
+            if (stream != null)
+                stream.Close();//отключение потока
+            if (client != null)
+                client.Close();//отключение клиента
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// Событие при нажатии на кнопку применить
+        /// </summary>
+        private void ApplyButton_Click(object sender, EventArgs e)
+        {
+            if (NameTextBox.Text != string.Empty)
+            {
+                NameTextBox.Enabled = false;
+                ApplyButton.Enabled = false;
+                MyTextBox.Enabled = true;
+                SendButton.Enabled = true;
+                this.Text = "Клиент: " + NameTextBox.Text;
+                userName = NameTextBox.Text;
+                Connection();
+            }
+            else
+            {
+                MessageBox.Show("Необходимо заполнить поле имя.",
+                    "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        /// <summary>
+        /// Событие при нажатии на кнопку отправить.
+        /// </summary>
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            SendMessage();
+        }
+    }
+}
